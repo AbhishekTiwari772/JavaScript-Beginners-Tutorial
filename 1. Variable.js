a1faa0f6aa82bdfc406780655f7d9b208d918518
Abhishek Tiwari <84613703+AbhishekTiwari772@users.noreply.github.com>
2021-05-31 12:50:14 +0530

    Update 1. Variable.js

--- 1. Variable.js
+++ 1. Variable.js
@@ -1,3 +1,22 @@
 /*
 In this first file of JavaScript tutorial, we will learn about variables, the most core concept of any programming language.
+
+Variables are placehders or containers for values. As the name suggests, the value stored inside variables can be changed.
+
+To declare Variables in JavaScript, we need to use the keyword "let", followed by a variable name. Then we put the equal to sign or the assignment operator, followed by the value the variable should hold.[ let (variable name) = (value) ]
 */
+let user = "Abhishek" // The variable user will hold the value "Abhishek".
+/*
+There are some rules for creating variable names.
+  1. A variable name can include alphabets, numbers and 2 special characters, $ and _
+  2. A variable name can start with alphabets and the 2 special characters.(They can't start with numbers) [ 1name is an incorrect variable name]
+  3. Variable names should be sensible(OPTIONAL). [ a, b, c, x, etc. should not be used as variable name]
+*/
+
+/*
+The value stored in variables can be changed. Let's understand how.
+To change the value of a variable, use the following syntax:-
+(variable name) = (new value)
+*/
+user = "Tiwari" // The variable user will now hold the new value "Tiwari"
+

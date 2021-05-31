f1974572620a28695402272389fd290497266b74
Abhishek Tiwari <84613703+AbhishekTiwari772@users.noreply.github.com>
2021-05-31 12:50:32 +0530

    Create 2. Constants.js

---
+++ 2. Constants.js
@@ -0,0 +1,5 @@
+/*
+The value stored in a variable can be changed. Sometimes, we don't want this. For eg. if your code holds the value of pi, which has a fixed value, you want that value to be fixed througout your code.
+In this case, we use Constants. Constants are declared by using the word "const". If we try to change the value of a constant, we get an error.
+*/
+const pi = 3.14 // The constant pi will hold the value 3.14 which can't be changed.

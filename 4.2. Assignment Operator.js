c1b030fb428aad929eaa70df090c0a646cd06507
Abhishek Tiwari <84613703+AbhishekTiwari772@users.noreply.github.com>
2021-06-02 12:13:37 +0530

    Create 4.2. Assignment Operator.js

---
+++ 4.2. Assignment Operator.js
@@ -0,0 +1,36 @@
+/*
+Since the start of this course, we have been using the equal to sign to give a value to a variable.
+This equal to sign is called the assignment operator and is used to give or assign a value to a variable.
+*/
+
+/*
+
+Operator                  	Example	              Actual work                                    Use
+
+  =           	            x = y	                  x = y                   
+  
+  +=	                      x += y	              x = x + y                           adds a value to a variable
+  
+  -=	                      x -= y	              x = x - y                         subtracts a value from a variable
+  
+  *=	                      x *= y	              x = x * y                             multiplies a variable
+  
+  /=	                      x /= y	              x = x / y                              divides a variable
+  
+  %=	                      x %= y              	x = x % y                         assigns a remainder to a variable.
+  
+  <<=	                      x <<= y               x = x << y                    
+  
+  >>=	                      x >>= y               x = x >> y
+  
+  >>>=	                    x >>>= y            	x = x >>> y
+  
+  &=	                      x &= y	              x = x & y
+  
+  ^=	                      x ^= y	              x = x ^ y
+  
+  |=	                      x |= y	              x = x | y
+  
+  **=	                      x **= y           	  x = x ** y
+
+*/

8e01fb51226633eafc6269456583d01bb9b6092e
Abhishek Tiwari <84613703+AbhishekTiwari772@users.noreply.github.com>
2021-06-02 10:50:30 +0530

    Create 4. Operators.js

---
+++ 4. Operators.js
@@ -0,0 +1,13 @@
+/*
+Now we will learn about operators in JavaScript.
+Operators are used to perform operations on values.
+The different types of operators in JavaScript are:
+  1. Arithmetic operators
+  2. Assignment operators
+  3. Logical operators
+  4. Bitwise operators
+  5. Comparison operators
+  5. String operators
+  
+We will talk about each in detail as other subchapters.
+*/

de8cd769da8b82cdb8e08aa9cdf2a45b21ac3166
Abhishek Tiwari <84613703+AbhishekTiwari772@users.noreply.github.com>
2021-06-02 11:51:21 +0530

    Create 4.1. Arithmetic Operators.js

---
+++ 4.1. Arithmetic Operators.js
@@ -0,0 +1,23 @@
+/*
+The first type of opeartors is Arithmetic operators.
+This operator incudes:
+  1. Addition      (+)  - Used to add 2 or more values.
+  2. Subtraction   (-)  - Used to subtract 2 numbers.
+  3. Multipication (*)  - Used to multiply 2 or more values.
+  4. Division      (/)  - Used to divide 2 numbers.
+  5. Modulus       (%)  - Used to obtain the remainder after division
+*/
+a = 10
+b = 20
+add = a + b     // Adding a and b                   30
+dif = a - b     // Subtracting b from a            -10
+product = a * b // Multiplying a and b              200
+div = a / b     // Dividing a by b                  0.5
+mod = a % b     // Remainder after a divided by b   10
+
+/*
+We can add 2 or more strings also
+*/
+a = "Java"
+b = "Script"
+add = a + b // Adding Java and Script will result "JavaScript". There will be no space between the added strings.
